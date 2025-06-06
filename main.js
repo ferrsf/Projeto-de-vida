c616993bafe573fa1c1920dc1da28e20324d0a87
ferrsf <161780122+ferrsf@users.noreply.github.com>
2025-06-06 09:09:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-06-29T00:00:00");
 const tempoObjetivo2 = new Date("2026-05-25T00:00:00");
 const tempoObjetivo3 = new Date("2028-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2026-02-01T00:00:00");
+const tempoObjetivo4 = new Date("2029-12-30T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
